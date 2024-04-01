ff719c62c33dbae4c9d7a366e801ffa6eb207c93
peasantbird <bertrand.wong.xh@gmail.com>
2024-04-01 18:12:01 +0800

    Add schema for postgres database

---
+++ schema.sql
@@ -0,0 +1,27 @@
+-- Create Teachers Table
+CREATE TABLE IF NOT EXISTS teachers (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL
+);
+
+-- Create Students Table
+CREATE TABLE IF NOT EXISTS students (
+    id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
+    is_suspended BOOLEAN NOT NULL DEFAULT false
+);
+
+-- Create Register Table
+CREATE TABLE IF NOT EXISTS register (
+    teacher_id INT NOT NULL,
+    student_id INT NOT NULL,
+   	PRIMARY KEY (teacher_id, student_id),
+    CONSTRAINT fk_teacher
+        FOREIGN KEY(teacher_id) 
+        REFERENCES teachers(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_student
+        FOREIGN KEY(student_id) 
+        REFERENCES students(id)
+        ON DELETE CASCADE
+);
