93fafc20da00b7ade4315df82479672206007636
peasantbird <bertrand.wong.xh@gmail.com>
2024-04-01 18:37:00 +0800

    Add gorm model

---
+++ pkg/db/models.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+    "gorm.io/gorm"
+)
+
+type Teacher struct {
+	gorm.Model
+	Email string `gorm:"unique;not null"`
+	Students []*Student `gorm:"many2many:teacher_students;"`
+}
+
+type Student struct {
+	gorm.Model
+	Email string `gorm:"unique;not null"`
+	IsSuspended bool `gorm:"default:false;not null"`
+	Teachers []*Teacher `gorm:"many2many:teacher_students;"`
+}
